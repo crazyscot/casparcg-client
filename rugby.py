12412d6a04974b93d5a726111f1e1a9a6a16b8b1
Ross Younger <onyx-commits@impropriety.org.uk>
2018-01-28 16:34:19 +1300

    rubgy: Cannot group penalty and drop goals together, they're different values in league...

--- rugby.py
+++ rugby.py
@@ -7,11 +7,15 @@ class RugbyScoreBug(scorebug.ScoreBug):
 
     def createSecondLine(self,sizer):
         line2 = wx.BoxSizer(wx.HORIZONTAL)
+        # Penalty and drop goal cannot be merged as league scores them differently...
+        # Conversion and penalty cannot be merged as union scores them differently...
         self.addButton(line2, 'TRY', lambda e: self.score_try(1), True)
         line2.AddSpacer(10)
         self.addButton(line2, 'CONV', lambda e: self.score_conv(1), True)
         line2.AddSpacer(10)
-        self.addButton(line2, 'KICK', lambda e: self.score_kick(1), True)
+        self.addButton(line2, 'PEN', lambda e: self.score_pen(1), True)
+        line2.AddSpacer(10)
+        self.addButton(line2, 'DROP', lambda e: self.score_drop(1), True)
 
         line2.AddStretchSpacer(1)
         line2.Add(wx.StaticText(self, label='Remember to press Update'))
@@ -21,7 +25,9 @@ class RugbyScoreBug(scorebug.ScoreBug):
         line2.AddSpacer(10)
         self.addButton(line2, 'CONV', lambda e: self.score_conv(2), True)
         line2.AddSpacer(10)
-        self.addButton(line2, 'KICK', lambda e: self.score_kick(2), True)
+        self.addButton(line2, 'PEN', lambda e: self.score_pen(2), True)
+        line2.AddSpacer(10)
+        self.addButton(line2, 'DROP', lambda e: self.score_drop(2), True)
 
         sizer.AddStretchSpacer(1)
         sizer.AddSpacer(10)
@@ -38,5 +44,7 @@ class RugbyScoreBug(scorebug.ScoreBug):
         self.score(team, 5)
     def score_conv(self, team):
         self.score(team, 2)
-    def score_kick(self, team):
+    def score_pen(self, team):
+        self.score(team, 3)
+    def score_drop(self, team):
         self.score(team, 3)
