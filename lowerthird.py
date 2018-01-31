6c3d213a5d744c38211496dac4105f03d17f3958
Ross Younger <onyx-commits@impropriety.org.uk>
2018-01-31 15:34:31 +1300

    lowerthird: Remove commonised code, add CUT OFF button

--- lowerthird.py
+++ lowerthird.py
@@ -45,9 +45,11 @@ class LowerThird(wx.StaticBox, Widget):
         sizer.Add(self.line2, flag=wx.EXPAND)
 
         bFadeOn = wx.Button(self, label='TAKE')
-        bFadeOn.Bind(wx.EVT_BUTTON, self.do_fade_on)
-        bFadeOff = wx.Button(self, label='OFF')
-        bFadeOff.Bind(wx.EVT_BUTTON, self.do_fade_off)
+        bFadeOn.Bind(wx.EVT_BUTTON, self.do_anim_on)
+        bFadeOff = wx.Button(self, label='ANIM OFF')
+        bFadeOff.Bind(wx.EVT_BUTTON, self.do_anim_off)
+        bCutOff = wx.Button(self, label='CUT OFF')
+        bCutOff.Bind(wx.EVT_BUTTON, self.do_remove)
         bUpdate = wx.Button(self, label='Update')
         bUpdate.Bind(wx.EVT_BUTTON, self.do_update)
 
@@ -69,6 +71,8 @@ class LowerThird(wx.StaticBox, Widget):
         inner.AddStretchSpacer(1)
         inner.Add(bFadeOff)
         inner.AddStretchSpacer(1)
+        inner.Add(bCutOff)
+        inner.AddStretchSpacer(1)
         inner.Add(bUpdate)
 
 
@@ -77,13 +81,6 @@ class LowerThird(wx.StaticBox, Widget):
         sizer.AddSpacer(20)
 
         sizer.AddStretchSpacer()
-
-    def channel(self):
-        return self.parent.channel()
-    def layer(self):
-        return self.config.get_int(self.config_section, configurable.Layer.label, LowerThird.my_default_config[configurable.Layer.label])
-    def template(self):
-        return self.config.get(self.config_section, configurable.Template.label, LowerThird.my_default_config[configurable.Template.label])
 
     def fg(self):
         return self.config.get(self.config_section, LowerThird.OPTION_FG, '#ffffff')
@@ -97,14 +94,6 @@ class LowerThird(wx.StaticBox, Widget):
             'colourB': self.cp.get_bg(),
             'colourA': self.cp.get_fg(),
             })
-
-    def do_fade_on(self, event):
-        # CG channel ADD layer template 1 data
-        self.parent.transact('CG %d-%d ADD 1 %s 1 %s'%(self.channel(), self.layer(), amcp.quote(self.template()), self.templateData()))
-
-    def do_fade_off(self, event):
-        # CG channel STOP layer
-        self.parent.transact('CG %d-%d STOP 1'%(self.channel(), self.layer()))
 
     def do_update(self,e):
         # CG channel UPDATE layer data
