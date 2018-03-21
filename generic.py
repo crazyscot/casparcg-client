9d3e6b150851200946e6daf64356241d34b1aa07
CasparCG User <casparcg@obtruck>
2018-03-21 18:06:26 +1300

    Create wrapper for generic client

---
+++ generic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+#
+# This file is part of Mediary's Caspar Client.
+# Copyright (C) 2018 Mediary Limited. All rights reserved.
+#
+
+'''
+Entrypoint for the generic client window
+
+To make this work:
+    * You MUST be using Caspar 2.1.0beta or later, 2.0 isn't good enough
+    * Install the templates to the caspar server's template directory
+'''
+
+import wxclient
+
+wxclient.run_app(None)
