ab12a6d07648fd6acb04b374cc0bc13c5fbf6807
Ross Younger <crazyscot@gmail.com>
2021-06-30 21:30:14 +1200

    update version tag

--- globalwidget.py
+++ globalwidget.py
@@ -17,7 +17,7 @@ from configurable import ConfigItem, IntConfigItem, classproperty
 from widget import Widget
 from wx.lib.wordwrap import wordwrap
 
-VERSION='0.3.0+'
+VERSION='0.3.1+'
 
 Server=ConfigItem('Server', 'IP address or host name of the CasparCG server to connect to')
 Port=IntConfigItem('Port', 'Port to connect to (usually 5250)')
